eb239bd7c31c84c6352267e7f2f6ea9f8268754d
ByeoungJoon Jeon <juni8453@naver.com>
2022-05-11 16:05:58 +0900

    :sparkles: feat : JpaRepository 제네릭 추가<클래스, 타입>(#4)
    추가하지 않았던 <클래스, 타입> 제네릭 추가

--- src/main/java/jpaproject/cafe/repository/ArticleRepository.java
+++ src/main/java/jpaproject/cafe/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package jpaproject.cafe.repository;
 
+import jpaproject.cafe.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
