611d3131eff8573beae2fc3cacb3b36e15e4797e
honeySleepr <gruzzimo@naver.com>
2022-05-12 18:25:11 +0900

    test :white_check_mark:: 게시글 수정 테스트 수정, 삭제 테스트 추가(#15)
    - 기존 테스트에서 save 메소드를 통해 가져온 id 대신 하드코딩 된
    id를 사용하였다가 테스트가 실패하는 문제가 발생하였다
    - 테스트 진행 중 Rollback을 통해 데이터는 삭제되더라도 Auto Increment 되는
    id 값은 초기화가 안되어 생기는 문제였다

--- src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
+++ src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
@@ -1,5 +1,11 @@
 package jpaproject.cafe.article;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import jpaproject.cafe.article.dto.ArticleCreateDto;
 import jpaproject.cafe.article.dto.ArticleUpdateDto;
 import org.junit.jupiter.api.DisplayName;
@@ -8,44 +14,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.NoSuchElementException;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @Transactional
 class ArticleServiceTest {
 
-    @Autowired
-    private ArticleRepository articleRepository;
-    @Autowired
-    private ArticleService articleService;
+	@Autowired
+	private ArticleRepository articleRepository;
+	@Autowired
+	private ArticleService articleService;
 
-    @Test
-    @DisplayName("클라이언트에서 CreateDto 로 넘긴 데이터를 Article 엔티티로 변환되어야 한다.")
-    void dtoToEntity() {
-        // given
-        ArticleCreateDto dto = new ArticleCreateDto("title", "content");
+	@Test
+	@DisplayName("클라이언트에서 CreateDto 로 넘긴 데이터를 Article 엔티티로 변환되어야 한다.")
+	void dtoToEntity() {
+		// given
+		ArticleCreateDto dto = new ArticleCreateDto("title", "content");
 
+		// when
+		Long id = articleService.save(dto);
+
+		Article findArticle = articleRepository.findById(id).get();
+		// then
+		assertThat(findArticle.getTitle()).isEqualTo("title");
+		assertThat(findArticle.getContent()).isEqualTo("content");
+	}
+
+	@Test
+	@DisplayName("update 시 존재하지 않는 id 를 클라이언트에서 받았을 경우, 예외가 터져야 한다.")
+	void updateExceptionTest() {
+		// given
+		ArticleCreateDto dto = new ArticleCreateDto("title", "content");
+		Long id = articleService.save(dto);
+		ArticleUpdateDto updateDto = new ArticleUpdateDto("수정 title", "수정 content");
+
+		// when, then
+		assertThrows(IllegalArgumentException.class,
+			() -> articleService.update(id + 1, updateDto));
+	}
+
+	@Test
+	@DisplayName("update 시 Entity의 정보가 변경돼야한다.")
+	void updateTest() {
+		// given
+		ArticleCreateDto createDto = new ArticleCreateDto("title", "content");
+		Long id = articleService.save(createDto);
+		ArticleUpdateDto updateDto = new ArticleUpdateDto("수정 title", "수정 content");
+		articleService.update(id, updateDto);
+
+		Article findArticle = articleRepository.findById(id).get();
+
+		// when, then
+		assertThat(findArticle.getTitle()).isEqualTo(updateDto.getTitle());
+		assertThat(findArticle.getContent()).isEqualTo(updateDto.getContent());
+	}
+
+	@Test
+	@DisplayName("delete 시 DB에서 엔티티가 삭제된다.")
+	public void deleteTest() {
+		// given
+		ArticleCreateDto createDto = new ArticleCreateDto("title", "content");
+		Long id = articleService.save(createDto);
         // when
-        articleService.save(dto);
-        Article findArticle = articleRepository.findById(1L).get();
+        articleService.delete(id);
 
         // then
-        assertThat(findArticle.getTitle()).isEqualTo("title");
-        assertThat(findArticle.getContent()).isEqualTo("content");
-    }
-
-    @Test
-    @DisplayName("update 시 존재하지 않는 id 를 클라이언트에서 받았을 경우, 예외가 터져야 한다.")
-    void updateExceptionTest() {
-        // given
-        ArticleCreateDto dto = new ArticleCreateDto("title", "content");
-        articleService.save(dto);
-        ArticleUpdateDto updateDto = new ArticleUpdateDto("수정 title", "수정 content");
-
-        // when, then
-        assertThrows(IllegalArgumentException.class, ()-> articleService.update(2L, updateDto));
-    }
+        assertThrows(NoSuchElementException.class, () -> articleRepository.findById(id).get());
+	}
 }
