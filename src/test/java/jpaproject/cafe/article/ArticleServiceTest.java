69ff63c8218232879dd17d66cb71b9a571bffbfc
ByeoungJoon Jeon <juni8453@naver.com>
2022-05-12 16:11:32 +0900

    feat :white_check_mark:: ArticleUpdateDto 추가(#15)
    - Article update 시 엉뚱한 id 값을 받아왔다면 예외가 터지도록 테스트 메서드를 추가

--- src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
+++ src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
@@ -1,20 +1,26 @@
 package jpaproject.cafe.article;
 
 import jpaproject.cafe.article.dto.ArticleCreateDto;
+import jpaproject.cafe.article.dto.ArticleUpdateDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class ArticleServiceTest {
 
-    @Autowired private  ArticleRepository articleRepository;
-    @Autowired private  ArticleService articleService;
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
+    private ArticleService articleService;
 
     @Test
     @DisplayName("클라이언트에서 CreateDto 로 넘긴 데이터를 Article 엔티티로 변환되어야 한다.")
@@ -29,5 +35,17 @@ class ArticleServiceTest {
         // then
         assertThat(findArticle.getTitle()).isEqualTo("title");
         assertThat(findArticle.getContent()).isEqualTo("content");
+    }
+
+    @Test
+    @DisplayName("update 시 존재하지 않는 id 를 클라이언트에서 받았을 경우, 예외가 터져야 한다.")
+    void updateExceptionTest() {
+        // given
+        ArticleCreateDto dto = new ArticleCreateDto("title", "content");
+        articleService.save(dto);
+        ArticleUpdateDto updateDto = new ArticleUpdateDto("수정 title", "수정 content");
+
+        // when, then
+        assertThrows(IllegalStateException.class, ()-> articleService.update(2L, updateDto));
     }
 }
