a7bcde491656f0a96dadb8688e20659f2a17cc9a
ByeoungJoon Jeon <juni8453@naver.com>
2022-05-12 16:40:47 +0900

    feat :sparkles:: 예외 처리 로직 추가(#15)
    - GloblaExcpeiton, RestResponse 클래스 추가

---
+++ src/main/java/jpaproject/cafe/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package jpaproject.cafe.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<RestResponse> IllegalArgumentExceptionMethod(IllegalArgumentException exception) {
+
+        String errorMessage = exception.getMessage();
+        log.error("errorMessage = {}", errorMessage);
+
+        RestResponse restResponse = RestResponse.of(errorMessage);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+    }
+}
