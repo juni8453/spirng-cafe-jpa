f887f69768f58e890ec5aa682c8f364430ffb9fd
honeySleepr <gruzzimo@naver.com>
2022-05-12 14:25:26 +0900

    feat :sparkles:: 엔티티 Auditing 기능 추가(#15)
    - Article 생성/수정 시간 저장 기능 추가

--- src/main/java/jpaproject/cafe/article/Article.java
+++ src/main/java/jpaproject/cafe/article/Article.java
@@ -1,18 +1,28 @@
 package jpaproject.cafe.article;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import jpaproject.cafe.member.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "title", "content", "createdDateTime", "lastModifiedDateTime"})
 public class Article {
 
 	@Id @GeneratedValue
@@ -26,7 +36,7 @@ public class Article {
 	private String content;
 
 	@CreatedDate
-	@Column(nullable = false)
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdDateTime;
 
 	@LastModifiedDate
@@ -37,7 +47,7 @@ public class Article {
 	@JoinColumn(name = "MEMBER_ID")
 	private Member member;
 
-	public Article(String title, String content, LocalDateTime createdDateTime, LocalDateTime lastModifiedDateTime, Member member) {
+	public Article(String title, String content, Member member) {
 		this.title = title;
 		this.content = content;
 		this.createdDateTime = createdDateTime;
