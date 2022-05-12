e5a55c0dda97965e63eb4dd65d8a0ffbf72155ad
ByeoungJoon Jeon <juni8453@naver.com>
2022-05-12 15:22:50 +0900

    feat :sparkles:: ArticleServiceTest 클래스 생성(#15)
    - Article Service 단의 테스트를 위해 클래스 생성
    - dtoToEntity() 테스트 진행

--- src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
+++ src/test/java/jpaproject/cafe/article/ArticleServiceTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package jpaproject.cafe.article;
+
+import jpaproject.cafe.article.dto.ArticleCreateDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class ArticleServiceTest {
-  
+
+    @Autowired private  ArticleRepository articleRepository;
+    @Autowired private  ArticleService articleService;
+
+    @Test
+    @DisplayName("클라이언트에서 CreateDto 로 넘긴 데이터를 Article 엔티티로 변환되어야 한다.")
+    void dtoToEntity() {
+        // given
+        ArticleCreateDto dto = new ArticleCreateDto("title", "content");
+
+        // when
+        articleService.save(dto);
+        Article findArticle = articleRepository.findById(1L).get();
+
+        // then
+        assertThat(findArticle.getTitle()).isEqualTo("title");
+        assertThat(findArticle.getContent()).isEqualTo("content");
+    }
 }
