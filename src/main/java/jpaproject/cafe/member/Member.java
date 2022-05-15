553deb4169e59fe8d676895feb2cf3537664cabc
honeySleepr <gruzzimo@naver.com>
2022-05-15 11:16:47 +0900

    chore :adhesive_bandage:: Member login 필드 초기화 방식 변경(#25)
    - 생성자 대신 @Prepersist 사용하는게 더 명시적인 느낌이 드는 것 같다

--- src/main/java/jpaproject/cafe/member/Member.java
+++ src/main/java/jpaproject/cafe/member/Member.java
@@ -1,11 +1,16 @@
 package jpaproject.cafe.member;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import jpaproject.cafe.member.dto.MemberInfoDto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
@@ -21,11 +26,18 @@ public class Member {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private MemberType memberType;
+	@Column(columnDefinition = "boolean default true")
 	private boolean login;
 
 	public Member(MemberInfoDto memberInfoDto, boolean normalUser) {
 		this.memberName = memberInfoDto.getName();
 		this.memberType = normalUser ? MemberType.USER : MemberType.ADMIN;
+	}
+
+	// default true로 설정해주어도 JPA는 그대로 null로 넣어버려서 이렇게 설정해주거나 그냥 초기화를 시켜줘야함
+	// @DynamicInsert 적용해봤으나 안됨
+	@PrePersist
+	public void prePersist() {
 		this.login = true;
 	}
 
