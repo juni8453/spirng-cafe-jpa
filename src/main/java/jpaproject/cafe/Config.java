dfd285a73c3eab843c821e919231d011146dc88e
honeySleepr <gruzzimo@naver.com>
2022-05-17 12:47:46 +0900

    feat :sparkles:: CORS mapping 설정 추가(#29)
    - Config 클래스에서 WebMvcConfigurer를 implement 하고 addCorsMappings
    오버라이딩

--- src/main/java/jpaproject/cafe/Config.java
+++ src/main/java/jpaproject/cafe/Config.java
@@ -3,12 +3,22 @@ package jpaproject.cafe;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config {
+public class Config implements WebMvcConfigurer {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Override public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+			.allowedHeaders("*")
+			.maxAge(3600);
+	}
 }
