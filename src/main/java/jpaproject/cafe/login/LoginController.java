cc5ef58bfb209ef3af0cac0f19efd6c491db3b26
honeySleepr <gruzzimo@naver.com>
2022-05-17 12:54:18 +0900

    feat :sparkles:: 로그인 된 후 응답으로 username 반환 기능 추가(#26)
    - Redirect 주소는 React로 만든 페이지(localhost:3000)로 보내지도록 함

--- src/main/java/jpaproject/cafe/login/LoginController.java
+++ src/main/java/jpaproject/cafe/login/LoginController.java
@@ -1,15 +1,17 @@
 package jpaproject.cafe.login;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import jpaproject.cafe.login.dto.LoginService;
 import jpaproject.cafe.login.dto.Token;
-
 import jpaproject.cafe.member.dto.OauthMemberInfo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -28,14 +30,30 @@ public class LoginController {
     }
 
     @GetMapping("/login/oauth")
-    public ResponseEntity<OauthMemberInfo> githubRedirect(
-            @RequestParam String code,
-            @RequestParam String state) {
+    public ResponseEntity<String> githubRedirect(
+        @RequestParam String code,
+        @RequestParam String state,
+        RedirectAttributes attributes) {
 
         Token accessTokenInfo = loginService.getAccessToken(code, state);
         OauthMemberInfo memberInfo = loginService.getOauthMemberInfo(accessTokenInfo);
         loginService.saveMember(memberInfo);
-        return ResponseEntity.ok(memberInfo);
+
+        /*-----------------------------------*/
+
+        ResponseCookie springCookie = ResponseCookie.from("username", memberInfo.getLogin())
+            .path("/")
+            .build();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        try {
+            httpHeaders.setLocation(new URI("http://localhost:3000/"));
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("URI가 잘못되었습니다.");
+        }
+        httpHeaders.set("set-cookie", springCookie.toString());
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
     }
 
 
